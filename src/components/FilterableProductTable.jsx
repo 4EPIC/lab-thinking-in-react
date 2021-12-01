0e9b3f68b1d90bab4c246d76a756c59397a6acdd
Eric Esteban <eric_7@hotmail.com>
2021-12-01 21:08:21 +0100

    done

---
+++ src/components/FilterableProductTable.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+import ProductTable from './ProductTable';
+
+export default class FilterableProductTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: this.props.products,
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <h1>IronStore</h1>
+        <SearchBar products={this.state.products} />
+        <ProductTable products={this.state.products} />
+      </>
+    );
+  }
+}
+
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export default class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  formSubmitHandler(e) {
+    e.preventDefault();
+  }
+
+  formHandler() {}
+
+  render() {
+    console.log(`xxxxxxxxxxxxxxxx`);
+    console.log(this.props.products[0].name);
+    return (
+      <>
+        <h3>Search</h3>
+        <form onSubmit={this.formSubmitHandler}>
+          <input
+            onChange={this.formHandler}
+            type="text"
+            placeholder="Search.."
+            name="name"
+            value={this.state.name}
+          />
+        </form>
+        <p>have here a search box</p>
+        <div>
+          <input type="checkbox" id="onStock" name="onStock" />
+          <label for="onStock">Only show products on stock</label>
+        </div>
+      </>
+    );
+  }
+}
